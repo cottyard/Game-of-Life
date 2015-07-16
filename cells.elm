2d895a57b76d480e39f2b153120f928d4b742a07
cottyard <cottyard@gmail.com>
2015-07-16 12:37:22 +0800

    model GameState with Array

--- cells.elm
+++ cells.elm
@@ -4,6 +4,7 @@ import Graphics.Element exposing (Element, show)
 import Graphics.Collage exposing (Shape, collage, square, filled, Form, defaultLine, outlined, group, move)
 import Mouse
 import Window
+import Array exposing (Array, get, set)
 
 kCANVAS_WIDTH = 650
 kCANVAS_HEIGHT = 650
@@ -21,23 +22,33 @@ main : Element
 main = scene
 
 type CellState = Alive | Dead
-type alias GameState = List (List CellState)
+type alias GameState = Array (Array CellState)
+
+type alias ImmutableGameState = List (List CellState)
 
 initial_game_state : GameState
-initial_game_state = List.repeat kCELL_COUNT_H (List.repeat kCELL_COUNT_W Dead)
+initial_game_state = 
+    let blank_state = (Array.repeat kCELL_COUNT_H (Array.repeat kCELL_COUNT_W Dead))
+    in case get 3 blank_state of
+        Just line ->
+            set 3 (set 4 Alive line) blank_state
+        Nothing ->
+            blank_state
 
+make_immutable : GameState -> ImmutableGameState
+make_immutable game_state = Array.map Array.toList game_state |> Array.toList
 
 scene : Element
-scene = collage kCANVAS_WIDTH kCANVAS_HEIGHT (draw_cells initial_game_state -300 -300 kCELL_COUNT_W kCELL_COUNT_H)
+scene = collage kCANVAS_WIDTH kCANVAS_HEIGHT (draw_cells (make_immutable initial_game_state) -300 -300)
 
-draw_cells : GameState -> Int -> Int -> Int -> Int -> List Form
-draw_cells game_state w_begin h_begin w_cells h_cells =
+draw_cells : ImmutableGameState -> Int -> Int -> List Form
+draw_cells game_state w_begin h_begin =
     let iter game_state line_count =
         case game_state of
             [] -> []
             (line_state :: rest_lines_state) ->
                 let line_offset = h_begin + line_count * kCELL_SIZE
-                in draw_cells_in_line line_state line_offset w_begin w_cells
+                in draw_cells_in_line line_state line_offset w_begin
                    ++ iter rest_lines_state (line_count + 1)
     in iter game_state 0
 
@@ -49,8 +60,8 @@ create_cell state =
         Dead  -> dead_cell
 
 
-draw_cells_in_line : List CellState -> Int -> Int -> Int -> List Form
-draw_cells_in_line line_state line_offset first_cell_offset cells =
+draw_cells_in_line : List CellState -> Int -> Int -> List Form
+draw_cells_in_line line_state line_offset first_cell_offset =
     let iter line_state cell_count =
         case line_state of
             [] -> []
