695cd25b550cd85372067275ea4021448ddc1616
cottyard <cottyard@gmail.com>
2015-08-20 13:41:00 +0800

    add signals; support mouse click

--- reconstruction/Game.elm
+++ reconstruction/Game.elm
@@ -1,4 +1,4 @@
-module Game (draw) where
+module Game where
 
 import Graphics.Collage exposing (Form, move)
 import Array exposing (Array)
@@ -9,7 +9,6 @@ import Time exposing (Time)
 type alias Model = Array (Array Cell.Model)
 type CellCoord = CellCoord (Int, Int)
 type Update = CellClick CellCoord | TimeUp Time
-
 
 cell_count_w = 100
 cell_count_h = 50
@@ -31,16 +30,15 @@ revertCell (CellCoord (x, y)) model =
 
 -- view
 
-draw : List Form
-draw = 
-  zipWith_Iter (\f c -> f c)
+draw : Model -> List Form
+draw model = 
+  zipWith_iter (\f c -> f c)
                (List.concat arrangementFuncs)
-               (List.concat <| drawCells init)
+               (List.concat <| drawCells model)
 
 drawCells : Model -> List (List Form)
 drawCells model =
   Array.map (Array.toList << Array.map Cell.draw) model |> Array.toList
-
 
 move_Int : (Int, Int) -> Form -> Form
 move_Int (x, y) = 
@@ -56,7 +54,6 @@ rangeIntList : Int -> Int -> Int -> List Int
 rangeIntList begin step size =
   List.map ((*) step >> (+) begin) [0..size - 1]
 
-
 -- Elm 0.15 doesn't support Tail Call Elimination; abandoning this function
 zipWith : (a -> b -> c) -> List a -> List b -> List c
 zipWith f l1 l2 =
@@ -65,15 +62,25 @@ zipWith f l1 l2 =
     (x::xs) -> let (y::ys) = l2 in
       f x y :: zipWith f xs ys
 
-zipWith_Iter : (a -> b -> c) -> List a -> List b -> List c
-zipWith_Iter f l1 l2 = trampoline <| zipWith_Iter' f l1 l2 []
+zipWith_iter : (a -> b -> c) -> List a -> List b -> List c
+zipWith_iter f l1 l2 = trampoline <| zipWith_iter' f l1 l2 []
 
-zipWith_Iter' : (a -> b -> c) -> List a -> List b -> List c -> Trampoline (List c)
-zipWith_Iter' f l1 l2 acc =
+zipWith_iter' : (a -> b -> c) -> List a -> List b -> List c -> Trampoline (List c)
+zipWith_iter' f l1 l2 acc =
   case l1 of
     []      -> Trampoline.Done acc
     (x::xs) -> let (y::ys) = l2 in
-      Trampoline.Continue (\() -> zipWith_Iter' f xs ys (f x y::acc))
+      Trampoline.Continue (\() -> zipWith_iter' f xs ys (f x y::acc))
+
+mousePosToCellCoord : (Int, Int) -> Maybe CellCoord
+mousePosToCellCoord (x_raw, y_raw) =
+  let x_calib = 50
+      y_calib = 765
+  in let x_coor = (x_raw - x_calib + Cell.size) // Cell.size - 1
+         y_coor = (y_calib - y_raw + Cell.size) // Cell.size - 1
+     in if | y_coor >= cell_count_h || y_coor < 0 ||
+             x_coor >= cell_count_w || x_coor < 0 -> Nothing
+           | otherwise -> Just (CellCoord (x_coor, y_coor))
 
 -- update
 
--- reconstruction/Main.elm
+++ reconstruction/Main.elm
@@ -1,11 +1,65 @@
 import Game
-import Graphics.Element exposing (Element)
+import Graphics.Element as GElem exposing (Element)
 import Graphics.Collage exposing (collage, Form, move)
+import Mouse
+import Time exposing (Time)
+import Signal exposing ((<~))
 
-(canvas_width, canvas_height) = (1800, 800)
+(canvas_width, canvas_height) = (1600, 800)
 
-main : Element
-main = collage canvas_width canvas_height (calibrate (-canvas_width / 2 + 50, -canvas_height / 2 + 50) Game.draw)
+main : Signal Element
+main = layout_live (game_live |> draw_live |> scene_live) 
+                   info_live
+
+-- signals
+
+mouseClick_live : Signal (Int, Int)
+mouseClick_live =
+  Signal.sampleOn Mouse.clicks Mouse.position
+
+clock : Signal Time
+clock =
+  Signal.constant Time.second
+  -- Time.fpsWhen 1 timerStateChanged
+
+cellClick_live : Signal Game.CellCoord
+cellClick_live =
+  Signal.filterMap Game.mousePosToCellCoord (Game.CellCoord (0, 0)) mouseClick_live
+
+game_live : Signal Game.Model
+game_live =
+  Signal.foldp Game.update Game.init incomingUpdate
+
+info_live : Signal Element
+info_live =
+  GElem.show <~ Mouse.position
+
+incomingUpdate : Signal Game.Update
+incomingUpdate =
+  Signal.merge (Game.CellClick <~ cellClick_live) (Game.TimeUp <~ clock)
+
+scene_live : Signal (List Form) -> Signal Element
+scene_live =
+  Signal.map scene
+
+draw_live : Signal (Game.Model) -> Signal (List Form)
+draw_live =
+  Signal.map Game.draw
+
+layout_live : Signal Element -> Signal Element -> Signal Element
+layout_live =
+  Signal.map2 layout
+
+----
+
+layout : Element -> Element -> Element
+layout e1 e2 =
+  GElem.flow GElem.right [e1, e2]
+
+scene : List Form -> Element
+scene forms =
+  collage canvas_width canvas_height (
+    calibrate (-canvas_width / 2 + 50, -canvas_height / 2 + 50) forms)
 
 calibrate : (Float, Float) -> List Form -> List Form
 calibrate offset forms =
